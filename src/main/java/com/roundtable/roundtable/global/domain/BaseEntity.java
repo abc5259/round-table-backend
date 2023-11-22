c0cf540f690bc681eb6ecb88e1a841282632704c
LeeJaeHoon <dlwogns3413@naver.com>
2023-11-23 03:36:50 +0900

    feat: BaseEntity class 구현

---
+++ src/main/java/com/roundtable/roundtable/global/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.roundtable.roundtable.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
