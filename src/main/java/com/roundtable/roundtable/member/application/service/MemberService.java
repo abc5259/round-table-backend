647c660d5eb5a0385ca361fc6536c8fd134cfe21
LeeJaeHoon <dlwogns3413@naver.com>
2023-11-27 15:35:07 +0900

    feat: 이메일 인증 코드 확인 로직 구현

--- src/main/java/com/roundtable/roundtable/member/application/service/MemberService.java
+++ src/main/java/com/roundtable/roundtable/member/application/service/MemberService.java
@@ -5,15 +5,14 @@ import com.roundtable.roundtable.member.application.authcode.AuthCodeStoreStrate
 import com.roundtable.roundtable.member.application.dto.EmailDto;
 import com.roundtable.roundtable.member.exception.MemberException.EmailDuplicatedException;
 import com.roundtable.roundtable.member.domain.MemberRepository;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
@@ -21,6 +20,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final AuthCodeStoreStrategy authCodeStoreStrategy;
 
+    @Transactional(readOnly = true)
     public void sendCodeToEmail(final EmailDto emailDto) {
         this.checkDuplicatedEmail(emailDto.email());
         String title = "Round Table 이메일 인증 번호";
--- src/main/java/com/roundtable/roundtable/member/presentation/controller/MemberController.java
+++ src/main/java/com/roundtable/roundtable/member/presentation/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.roundtable.roundtable.member.presentation.controller;
 
+import com.roundtable.roundtable.member.application.authcode.AuthCode;
+import com.roundtable.roundtable.member.application.authcode.AuthCodeStoreStrategy;
 import com.roundtable.roundtable.member.application.dto.EmailDto;
 import com.roundtable.roundtable.member.application.service.MemberService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final AuthCodeStoreStrategy authCodeStoreStrategy;
 
     @PostMapping("/emails/verification-requests")
-    public ResponseEntity sendMessage(@Valid final EmailDto emailDto) {
+    public ResponseEntity sendAuthCode(@Valid final EmailDto emailDto) {
         memberService.sendCodeToEmail(emailDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/emails/verification-requests")
+    public ResponseEntity isCorrectAuthCode(@Valid @NotBlank String code) {
+        boolean isCorrect = authCodeStoreStrategy.isCorrectAuthCode(new AuthCode(code));
+        if(!isCorrect) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity(HttpStatus.OK);
     }
 }
