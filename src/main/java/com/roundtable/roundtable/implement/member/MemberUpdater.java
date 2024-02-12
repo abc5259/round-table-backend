283e9b698f7945a3f5b32daddfc700adad2f58c9
LeeJaeHoon <dlwogns3413@naver.com>
2024-02-13 03:15:28 +0900

    test: MemberUpdaterTest 완료

--- src/main/java/com/roundtable/roundtable/implement/member/MemberUpdater.java
+++ src/main/java/com/roundtable/roundtable/implement/member/MemberUpdater.java
@@ -4,8 +4,10 @@ import com.roundtable.roundtable.entity.house.House;
 import com.roundtable.roundtable.entity.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class MemberUpdater {
 
