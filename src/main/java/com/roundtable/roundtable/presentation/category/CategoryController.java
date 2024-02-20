c4dadad95ed629c4d59d18fe3b789fb70c7c845e
LeeJaeHoon <dlwogns3413@naver.com>
2024-02-20 22:17:54 +0900

    feat: category 생성 api 완성

---
+++ src/main/java/com/roundtable/roundtable/presentation/category/CategoryController.java
@@ -0,0 +1,35 @@
+package com.roundtable.roundtable.presentation.category;
+
+import com.roundtable.roundtable.business.category.CategoryService;
+import com.roundtable.roundtable.entity.member.Member;
+import com.roundtable.roundtable.presentation.category.request.CreateCategoryRequest;
+import com.roundtable.roundtable.presentation.support.argumentresolver.Login;
+import com.roundtable.roundtable.presentation.support.response.Response;
+import com.roundtable.roundtable.presentation.support.response.SuccessResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Response<Long>> createCategory(
+            @Login Member loginMember,
+            @Valid @RequestBody CreateCategoryRequest createCategoryRequest
+            ) {
+        Long categoryId = categoryService.createCategory(
+                createCategoryRequest.toCreateCategory(loginMember.getHouse())
+        );
+        return ResponseEntity.ok(SuccessResponse.from(categoryId));
+    }
+
+}
---
+++ src/main/java/com/roundtable/roundtable/presentation/category/request/CreateCategoryRequest.java
@@ -0,0 +1,25 @@
+package com.roundtable.roundtable.presentation.category.request;
+
+import com.roundtable.roundtable.entity.house.House;
+import com.roundtable.roundtable.implement.category.CreateCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+public record CreateCategoryRequest(
+        @NotBlank(message = "이름은 필수입니다.")
+        String name,
+        @NotEmpty(message = "포인트는 필수입니다.")
+        @Positive(message = "포인트는 0보다 커야합니다.")
+        Integer point
+) {
+
+
+    public CreateCategory toCreateCategory(House house) {
+        return new CreateCategory(
+                name,
+                point,
+                house
+        );
+    }
+}
