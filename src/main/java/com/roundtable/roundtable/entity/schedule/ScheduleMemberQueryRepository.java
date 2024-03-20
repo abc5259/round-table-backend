6a7eee8c554e9c1efd8929ac05affb1333c27de2
LeeJaeHoon <dlwogns3413@naver.com>
2024-03-20 21:00:00 +0900

    refactor: ScheduleMemberQueryRepository의 findMembersBySchedulesWithDivisionConditions 메소드 리팩터링

--- src/main/java/com/roundtable/roundtable/entity/schedule/ScheduleMemberQueryRepository.java
+++ src/main/java/com/roundtable/roundtable/entity/schedule/ScheduleMemberQueryRepository.java
@@ -1,11 +1,14 @@
 package com.roundtable.roundtable.entity.schedule;
 
 import static com.roundtable.roundtable.entity.member.QMember.*;
+import static com.roundtable.roundtable.entity.schedule.FrequencyType.*;
 import static com.roundtable.roundtable.entity.schedule.QSchedule.schedule;
 import static com.roundtable.roundtable.entity.schedule.QScheduleMember.*;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.NumberTemplate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.roundtable.roundtable.entity.schedule.dto.QScheduleMemberDetailDto;
 import com.roundtable.roundtable.entity.schedule.dto.ScheduleMemberDetailDto;
@@ -34,47 +37,66 @@ public class ScheduleMemberQueryRepository {
                 .fetch();
     }
 
-    public List<ScheduleMemberDetailDto> findScheduleMember(List<Schedule> schedules) {
-        NumberExpression<Integer> daysDiff = Expressions.numberTemplate(Integer.class,
-                "DATEDIFF({0}, {1})",
-                Expressions.currentDate(),
-                schedule.startDate);
+    public List<ScheduleMember> findMembersBySchedulesWithDivisionConditions(List<Schedule> schedules) {
 
         return queryFactory
-                .select(new QScheduleMemberDetailDto(
-                        scheduleMember.member.id,
-                        scheduleMember.member.name,
-                        scheduleMember.sequence
-                ))
+                .select(scheduleMember)
                 .from(scheduleMember)
                 .join(scheduleMember.member, member)
                 .join(scheduleMember.schedule, schedule)
-                .where(scheduleMember.schedule.in(schedules)
-                        .and(scheduleMember.schedule.divisionType.eq(DivisionType.FIX)
-                                .or(scheduleMember.schedule.divisionType.eq(DivisionType.ROTATION)
-                                                .and(scheduleMember.schedule.frequency.frequencyType.eq(FrequencyType.DAILY)
-                                                                .and(scheduleMember.sequence.eq(
-                                                                                daysDiff
-                                                                                        .divide(scheduleMember.schedule.frequency.frequencyInterval)
-                                                                                        .mod(scheduleMember.schedule.sequenceSize)
-                                                                                        .add(1)
-                                                                        )
-                                                                )
-                                                        )
-                                                .or(scheduleMember.schedule.frequency.frequencyType.eq(FrequencyType.WEEKLY)
-                                                        .and(scheduleMember.sequence.eq(
-                                                                        daysDiff
-                                                                                .divide(7)
-                                                                                .mod(scheduleMember.schedule.sequenceSize)
-                                                                                .add(1)
-                                                                )
-                                                        )
-                                                )
-                                                .or(scheduleMember.schedule.frequency.frequencyType.eq(FrequencyType.ONCE))
-                                )
-                        )
+                .where(schedule.in(schedules)
+                        .and(
+                                getScheduleDivisionTypeEq(DivisionType.FIX)
+                                .or(getScheduleDivisionTypeEq(DivisionType.ROTATION).and(getRotationCondition())))
                 )
                 .fetch();
+    }
+
+    private BooleanExpression getScheduleDivisionTypeEq(DivisionType divisionType) {
+        return scheduleMember.schedule.divisionType.eq(divisionType);
+    }
+
+    private BooleanExpression getRotationCondition() {
+
+        BooleanExpression dailyCondition = getScheduleFrequencyTypeEq(DAILY)
+                .and(getSequenceEq(calculateSequenceMatchCondition(schedule.frequency.frequencyInterval)));
+
+        BooleanExpression weeklyCondition = getScheduleFrequencyTypeEq(WEEKLY)
+                .and(getSequenceEq(calculateSequenceMatchCondition(7)));
+
+        BooleanExpression onceCondition = getScheduleFrequencyTypeEq(ONCE);
+
+        return dailyCondition.or(weeklyCondition).or(onceCondition);
+    }
+
+    private static BooleanExpression getScheduleFrequencyTypeEq(FrequencyType frequencyType) {
+        return schedule.frequency.frequencyType.eq(frequencyType);
+    }
+
+    private BooleanExpression getSequenceEq(NumberExpression<Integer> numberExpression) {
+        return scheduleMember.sequence.eq(numberExpression);
+    }
+
+    private NumberExpression<Integer> calculateSequenceMatchCondition(NumberExpression<Integer> divisor) {
+
+
+        return calculateDaysDifferenceFromCurrentDate().divide(divisor)
+                .mod(scheduleMember.schedule.sequenceSize)
+                .add(1);
+    }
+
+    private NumberExpression<Integer> calculateSequenceMatchCondition(int divisor) {
+
+        return calculateDaysDifferenceFromCurrentDate().divide(divisor)
+                .mod(scheduleMember.schedule.sequenceSize)
+                .add(1);
+    }
+
+    private NumberTemplate<Integer> calculateDaysDifferenceFromCurrentDate() {
+        return Expressions.numberTemplate(Integer.class,
+                "DATEDIFF({0}, {1})",
+                Expressions.currentDate(),
+                schedule.startDate);
     }
 
 }
