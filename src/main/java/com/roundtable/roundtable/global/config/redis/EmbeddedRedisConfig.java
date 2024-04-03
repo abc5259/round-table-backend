85c1cea96a6ad4eb22a7b97d050acdb13de9e41d
LeeJaeHoon <dlwogns3413@naver.com>
2024-04-03 22:20:13 +0900

    fix: RedisConfig test실행시 port충돌 해결

--- src/main/java/com/roundtable/roundtable/global/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/com/roundtable/roundtable/global/config/redis/EmbeddedRedisConfig.java
@@ -3,13 +3,15 @@ package com.roundtable.roundtable.global.config.redis;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
-@Profile("test") // profile이 local일때만 활성화
+@Profile("test") // profile이 test일때만 활성화
 @Configuration
+@Slf4j
 public class EmbeddedRedisConfig {
 
     @Value("${spring.data.redis.port}")
@@ -20,6 +22,7 @@ public class EmbeddedRedisConfig {
     @PostConstruct
     public void redisServer() throws IOException {
         redisServer = new RedisServer(redisPort);
+        log.info("EmbeddedRedis start port with " + redisPort);
         redisServer.start();
     }
 
