5485e9257476ad16cacf49e309ba2ea348d62aa6
LeeJaeHoon <dlwogns3413@naver.com>
2024-04-27 22:10:47 +0900

    fix: M1 EmbeddedRedis 문제 해결

--- src/main/java/com/roundtable/roundtable/global/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/com/roundtable/roundtable/global/config/redis/EmbeddedRedisConfig.java
@@ -1,12 +1,19 @@
 package com.roundtable.roundtable.global.config.redis;
 
+import com.roundtable.roundtable.global.exception.ApplicationException;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StringUtils;
 import redis.embedded.RedisServer;
 
 @Profile("test") // profile이 test일때만 활성화
@@ -21,15 +28,87 @@ public class EmbeddedRedisConfig {
 
     @PostConstruct
     public void redisServer() throws IOException {
-        redisServer = new RedisServer(redisPort);
-        log.info("EmbeddedRedis start port with " + redisPort);
-        redisServer.start();
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
+
+        if (isArmMac()) {
+            redisServer = new RedisServer(getRedisFileForArcMac(), port);
+        } else {
+            redisServer = RedisServer.builder()
+                    .port(port)
+                    .setting("maxmemory 128M")
+                    .build();
+        }
+
+        try {
+            redisServer.start();
+        } catch (Exception e) {
+            throw new ApplicationException(e.getMessage());
+        }
     }
 
     @PreDestroy
     public void stopRedis() {
         if (redisServer != null) {
             redisServer.stop();
+        }
+    }
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+
+        throw new ApplicationException("사용가능한 port가 없습니다.");
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스를 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int redisPort) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", redisPort);
+        String[] shell = {"/bin/sh", "-c", command};
+
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    /**
+     * 해당 Process가 현재 실행중인지 확인
+     */
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+        } catch (Exception e) {
+            throw new ApplicationException(e);
+        }
+        return StringUtils.hasText(pidInfo.toString());
+    }
+
+    private boolean isArmMac() {
+        return Objects.equals(System.getProperty("os.arch"), "aarch64") &&
+                Objects.equals(System.getProperty("os.name"), "Mac OS X");
+
+    }
+
+    private File getRedisFileForArcMac() {
+        try {
+            log.info("getRedisFileForArcMac");
+            return new ClassPathResource("binary/redis/redis-server-7.2.4-mac-arm64").getFile();
+        } catch (Exception e) {
+            throw new ApplicationException(e.getMessage());
         }
     }
 }
