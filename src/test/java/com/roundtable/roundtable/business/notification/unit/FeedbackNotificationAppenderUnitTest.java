9ac795cb89fd596806852e8e22a506d340bc52b4
LeeJaeHoon <dlwogns3413@naver.com>
2024-07-28 16:56:58 +0900

    test: FeedbackNotificationAppenderUnitTest append method test

--- src/test/java/com/roundtable/roundtable/business/notification/unit/FeedbackNotificationAppenderUnitTest.java
+++ src/test/java/com/roundtable/roundtable/business/notification/unit/FeedbackNotificationAppenderUnitTest.java
@@ -2,19 +2,15 @@ package com.roundtable.roundtable.business.notification.unit;
 
 import static org.mockito.BDDMockito.*;
 
-import com.roundtable.roundtable.business.chore.ChoreMemberReader;
-import com.roundtable.roundtable.business.chore.ChoreReader;
-import com.roundtable.roundtable.business.member.MemberReader;
 import com.roundtable.roundtable.business.notification.FeedbackNotificationAppender;
-import com.roundtable.roundtable.domain.chore.Chore;
-import com.roundtable.roundtable.domain.chore.ChoreMember;
+import com.roundtable.roundtable.business.notification.NotificationFactory;
 import com.roundtable.roundtable.domain.member.Member;
+import com.roundtable.roundtable.domain.notification.FeedbackNotification;
 import com.roundtable.roundtable.domain.notification.NotificationRepository;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,19 +18,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class FeedbackNotificationAppenderUnitTest {
 
-    @Mock
+    @InjectMocks
     private FeedbackNotificationAppender feedbackNotificationAppender;
 
-    @InjectMocks
-    private MemberReader memberReader;
+    @Mock
+    private NotificationFactory notificationFactory;
 
-    @InjectMocks
-    private ChoreReader choreReader;
-
-    @InjectMocks
-    private ChoreMemberReader choreMemberReader;
-
-    @InjectMocks
+    @Mock
     private NotificationRepository notificationRepository;
 
     @DisplayName("집안일을 완료한 맴버들에게 피드백 알림을 만든다.")
@@ -47,23 +37,25 @@ class FeedbackNotificationAppenderUnitTest {
         Long senderId = 1L;
 
         Member sender = Member.builder().id(1L).name("sender").build();
-        Chore chore = Chore.builder().id(1L).build();
         List<Member> receivers = List.of(
                 Member.builder().id(2L).name("sender").build(),
-                Member.builder().id(3L).name("sender").build(),
-                Member.builder().id(4L).name("sender").build()
+                Member.builder().id(3L).name("sender").build()
         );
 
-        given(memberReader.findById(feedbackId)).willReturn(sender);
-        given(choreReader.readById(choreId)).willReturn(chore);
-        given(choreMemberReader.readMembersByChoreId(choreId)).willReturn(receivers);
+        List<FeedbackNotification> feedbackNotifications = List.of(
+                FeedbackNotification.builder().sender(sender).receiver(receivers.get(0)).feedbackId(feedbackId).build(),
+                FeedbackNotification.builder().sender(sender).receiver(receivers.get(1)).feedbackId(feedbackId).build()
+        );
+        given(notificationFactory.createFeedbackNotifications(feedbackId, houseId, choreId, senderId)).willReturn(
+                feedbackNotifications
+        );
 
         //when
-
+        feedbackNotificationAppender.append(feedbackId, houseId, choreId, senderId);
 
         //then
-//        verify(notificationRepository, times(1)).saveAll();
-
+        verify(notificationFactory).createFeedbackNotifications(feedbackId, houseId, choreId, senderId);
+        verify(notificationRepository, times(1)).saveAll(feedbackNotifications);
     }
 
 }
